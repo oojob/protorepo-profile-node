7bd51ee29abe9c82d226da77fe4f062465293b1e
nirajgeorgian <niraj.kishore@stayology.com>
2020-05-08 09:29:04 +0530

    added base package

---
+++ service_grpc_pb.js
@@ -0,0 +1,218 @@
+// GENERATED CODE -- DO NOT EDIT!
+
+// Original file comments:
+//
+// Created on Sun Apr 28 2020
+//
+// Profile related messages.
+// This file is resposible for all operation related to profile.
+//
+// @author nirajgeorgian@oojob.io (Niraj Kishore)
+//
+// Copyright (c) 2020 - oojobs
+'use strict';
+var grpc = require('grpc');
+var services_profile_service_pb = require('./service_pb.js');
+var oojob_protobuf_system_pb = require('@oojob/oojob-protobuf/system_pb.js');
+var oojob_protobuf_metadata_pb = require('@oojob/oojob-protobuf/metadata_pb.js');
+var oojob_protobuf_place_pb = require('@oojob/oojob-protobuf/place_pb.js');
+var oojob_protobuf_health_pb = require('@oojob/oojob-protobuf/health_pb.js');
+var google_protobuf_timestamp_pb = require('google-protobuf/google/protobuf/timestamp_pb.js');
+
+function serialize_oojob_protobuf_DefaultResponse(arg) {
+  if (!(arg instanceof oojob_protobuf_system_pb.DefaultResponse)) {
+    throw new Error('Expected argument of type oojob.protobuf.DefaultResponse');
+  }
+  return new Buffer(arg.serializeBinary());
+}
+
+function deserialize_oojob_protobuf_DefaultResponse(buffer_arg) {
+  return oojob_protobuf_system_pb.DefaultResponse.deserializeBinary(new Uint8Array(buffer_arg));
+}
+
+function serialize_oojob_protobuf_HealthCheckRequest(arg) {
+  if (!(arg instanceof oojob_protobuf_health_pb.HealthCheckRequest)) {
+    throw new Error('Expected argument of type oojob.protobuf.HealthCheckRequest');
+  }
+  return new Buffer(arg.serializeBinary());
+}
+
+function deserialize_oojob_protobuf_HealthCheckRequest(buffer_arg) {
+  return oojob_protobuf_health_pb.HealthCheckRequest.deserializeBinary(new Uint8Array(buffer_arg));
+}
+
+function serialize_oojob_protobuf_HealthCheckResponse(arg) {
+  if (!(arg instanceof oojob_protobuf_health_pb.HealthCheckResponse)) {
+    throw new Error('Expected argument of type oojob.protobuf.HealthCheckResponse');
+  }
+  return new Buffer(arg.serializeBinary());
+}
+
+function deserialize_oojob_protobuf_HealthCheckResponse(buffer_arg) {
+  return oojob_protobuf_health_pb.HealthCheckResponse.deserializeBinary(new Uint8Array(buffer_arg));
+}
+
+function serialize_profile_ConfirmProfileRequest(arg) {
+  if (!(arg instanceof services_profile_service_pb.ConfirmProfileRequest)) {
+    throw new Error('Expected argument of type profile.ConfirmProfileRequest');
+  }
+  return new Buffer(arg.serializeBinary());
+}
+
+function deserialize_profile_ConfirmProfileRequest(buffer_arg) {
+  return services_profile_service_pb.ConfirmProfileRequest.deserializeBinary(new Uint8Array(buffer_arg));
+}
+
+function serialize_profile_Profile(arg) {
+  if (!(arg instanceof services_profile_service_pb.Profile)) {
+    throw new Error('Expected argument of type profile.Profile');
+  }
+  return new Buffer(arg.serializeBinary());
+}
+
+function deserialize_profile_Profile(buffer_arg) {
+  return services_profile_service_pb.Profile.deserializeBinary(new Uint8Array(buffer_arg));
+}
+
+function serialize_profile_ReadProfileRequest(arg) {
+  if (!(arg instanceof services_profile_service_pb.ReadProfileRequest)) {
+    throw new Error('Expected argument of type profile.ReadProfileRequest');
+  }
+  return new Buffer(arg.serializeBinary());
+}
+
+function deserialize_profile_ReadProfileRequest(buffer_arg) {
+  return services_profile_service_pb.ReadProfileRequest.deserializeBinary(new Uint8Array(buffer_arg));
+}
+
+function serialize_profile_ValidateEmailRequest(arg) {
+  if (!(arg instanceof services_profile_service_pb.ValidateEmailRequest)) {
+    throw new Error('Expected argument of type profile.ValidateEmailRequest');
+  }
+  return new Buffer(arg.serializeBinary());
+}
+
+function deserialize_profile_ValidateEmailRequest(buffer_arg) {
+  return services_profile_service_pb.ValidateEmailRequest.deserializeBinary(new Uint8Array(buffer_arg));
+}
+
+function serialize_profile_ValidateUsernameRequest(arg) {
+  if (!(arg instanceof services_profile_service_pb.ValidateUsernameRequest)) {
+    throw new Error('Expected argument of type profile.ValidateUsernameRequest');
+  }
+  return new Buffer(arg.serializeBinary());
+}
+
+function deserialize_profile_ValidateUsernameRequest(buffer_arg) {
+  return services_profile_service_pb.ValidateUsernameRequest.deserializeBinary(new Uint8Array(buffer_arg));
+}
+
+
+// *
+// ProfileService responsible for profile microservice operation.
+// more info can be found at : https://github.com/oojob/service-profile
+// project: github.com/oojob
+// how does this profile service work as a full microservice
+// - we need to create profile or in other term's signup
+// - then we can login to view profile and then perform profile operation
+var ProfileServiceService = exports.ProfileServiceService = {
+  // / ProfileService CreateProfile :- help's uo to create a profile
+  createProfile: {
+    path: '/profile.ProfileService/CreateProfile',
+    requestStream: false,
+    responseStream: false,
+    requestType: services_profile_service_pb.Profile,
+    responseType: services_profile_service_pb.Profile,
+    requestSerialize: serialize_profile_Profile,
+    requestDeserialize: deserialize_profile_Profile,
+    responseSerialize: serialize_profile_Profile,
+    responseDeserialize: deserialize_profile_Profile,
+  },
+  // / ProfileService ConfirmProfile :- help's us to verify the profile
+  confirmProfile: {
+    path: '/profile.ProfileService/ConfirmProfile',
+    requestStream: false,
+    responseStream: false,
+    requestType: services_profile_service_pb.ConfirmProfileRequest,
+    responseType: oojob_protobuf_system_pb.DefaultResponse,
+    requestSerialize: serialize_profile_ConfirmProfileRequest,
+    requestDeserialize: deserialize_profile_ConfirmProfileRequest,
+    responseSerialize: serialize_oojob_protobuf_DefaultResponse,
+    responseDeserialize: deserialize_oojob_protobuf_DefaultResponse,
+  },
+  // / ProfileService ReadProfile :- help's us to read the entire profile data
+  readProfile: {
+    path: '/profile.ProfileService/ReadProfile',
+    requestStream: false,
+    responseStream: false,
+    requestType: services_profile_service_pb.ReadProfileRequest,
+    responseType: services_profile_service_pb.Profile,
+    requestSerialize: serialize_profile_ReadProfileRequest,
+    requestDeserialize: deserialize_profile_ReadProfileRequest,
+    responseSerialize: serialize_profile_Profile,
+    responseDeserialize: deserialize_profile_Profile,
+  },
+  // / ProfileService UpdateProfile :- help's us to update the profile
+  updateProfile: {
+    path: '/profile.ProfileService/UpdateProfile',
+    requestStream: false,
+    responseStream: false,
+    requestType: services_profile_service_pb.Profile,
+    responseType: services_profile_service_pb.Profile,
+    requestSerialize: serialize_profile_Profile,
+    requestDeserialize: deserialize_profile_Profile,
+    responseSerialize: serialize_profile_Profile,
+    responseDeserialize: deserialize_profile_Profile,
+  },
+  // / ProfileService  ValidateUsername :- help's us to validate username
+  // / availability
+  validateUsername: {
+    path: '/profile.ProfileService/ValidateUsername',
+    requestStream: false,
+    responseStream: false,
+    requestType: services_profile_service_pb.ValidateUsernameRequest,
+    responseType: oojob_protobuf_system_pb.DefaultResponse,
+    requestSerialize: serialize_profile_ValidateUsernameRequest,
+    requestDeserialize: deserialize_profile_ValidateUsernameRequest,
+    responseSerialize: serialize_oojob_protobuf_DefaultResponse,
+    responseDeserialize: deserialize_oojob_protobuf_DefaultResponse,
+  },
+  // / ProfileService ValidateEmail :- help's us to validate email availability
+  validateEmail: {
+    path: '/profile.ProfileService/ValidateEmail',
+    requestStream: false,
+    responseStream: false,
+    requestType: services_profile_service_pb.ValidateEmailRequest,
+    responseType: oojob_protobuf_system_pb.DefaultResponse,
+    requestSerialize: serialize_profile_ValidateEmailRequest,
+    requestDeserialize: deserialize_profile_ValidateEmailRequest,
+    responseSerialize: serialize_oojob_protobuf_DefaultResponse,
+    responseDeserialize: deserialize_oojob_protobuf_DefaultResponse,
+  },
+  // / Used to check health status of the running microservice
+  check: {
+    path: '/profile.ProfileService/Check',
+    requestStream: false,
+    responseStream: false,
+    requestType: oojob_protobuf_health_pb.HealthCheckRequest,
+    responseType: oojob_protobuf_health_pb.HealthCheckResponse,
+    requestSerialize: serialize_oojob_protobuf_HealthCheckRequest,
+    requestDeserialize: deserialize_oojob_protobuf_HealthCheckRequest,
+    responseSerialize: serialize_oojob_protobuf_HealthCheckResponse,
+    responseDeserialize: deserialize_oojob_protobuf_HealthCheckResponse,
+  },
+  // / Used to check health status of the running microservice as stream
+  watch: {
+    path: '/profile.ProfileService/Watch',
+    requestStream: false,
+    responseStream: true,
+    requestType: oojob_protobuf_health_pb.HealthCheckRequest,
+    responseType: oojob_protobuf_health_pb.HealthCheckResponse,
+    requestSerialize: serialize_oojob_protobuf_HealthCheckRequest,
+    requestDeserialize: deserialize_oojob_protobuf_HealthCheckRequest,
+    responseSerialize: serialize_oojob_protobuf_HealthCheckResponse,
+    responseDeserialize: deserialize_oojob_protobuf_HealthCheckResponse,
+  },
+};
+
+exports.ProfileServiceClient = grpc.makeGenericClientConstructor(ProfileServiceService);
---
+++ service_pb.d.ts
@@ -0,0 +1,242 @@
+// package: profile
+// file: services/profile/service.proto
+
+import * as jspb from "google-protobuf";
+import * as oojob_protobuf_system_pb from "@oojob/oojob-protobuf/system_pb";
+import * as oojob_protobuf_metadata_pb from "@oojob/oojob-protobuf/metadata_pb";
+import * as oojob_protobuf_place_pb from "@oojob/oojob-protobuf/place_pb";
+import * as oojob_protobuf_health_pb from "@oojob/oojob-protobuf/health_pb";
+import * as google_protobuf_timestamp_pb from "google-protobuf/google/protobuf/timestamp_pb";
+
+export class Education extends jspb.Message {
+  getEducation(): string;
+  setEducation(value: string): void;
+
+  getShow(): boolean;
+  setShow(value: boolean): void;
+
+  serializeBinary(): Uint8Array;
+  toObject(includeInstance?: boolean): Education.AsObject;
+  static toObject(includeInstance: boolean, msg: Education): Education.AsObject;
+  static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+  static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
+  static serializeBinaryToWriter(message: Education, writer: jspb.BinaryWriter): void;
+  static deserializeBinary(bytes: Uint8Array): Education;
+  static deserializeBinaryFromReader(message: Education, reader: jspb.BinaryReader): Education;
+}
+
+export namespace Education {
+  export type AsObject = {
+    education: string,
+    show: boolean,
+  }
+}
+
+export class ProfileSecurity extends jspb.Message {
+  getPassword(): string;
+  setPassword(value: string): void;
+
+  getPasswordSalt(): string;
+  setPasswordSalt(value: string): void;
+
+  getPasswordHash(): string;
+  setPasswordHash(value: string): void;
+
+  getCode(): string;
+  setCode(value: string): void;
+
+  getCodeType(): string;
+  setCodeType(value: string): void;
+
+  getAccountType(): string;
+  setAccountType(value: string): void;
+
+  getVerified(): boolean;
+  setVerified(value: boolean): void;
+
+  serializeBinary(): Uint8Array;
+  toObject(includeInstance?: boolean): ProfileSecurity.AsObject;
+  static toObject(includeInstance: boolean, msg: ProfileSecurity): ProfileSecurity.AsObject;
+  static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+  static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
+  static serializeBinaryToWriter(message: ProfileSecurity, writer: jspb.BinaryWriter): void;
+  static deserializeBinary(bytes: Uint8Array): ProfileSecurity;
+  static deserializeBinaryFromReader(message: ProfileSecurity, reader: jspb.BinaryReader): ProfileSecurity;
+}
+
+export namespace ProfileSecurity {
+  export type AsObject = {
+    password: string,
+    passwordSalt: string,
+    passwordHash: string,
+    code: string,
+    codeType: string,
+    accountType: string,
+    verified: boolean,
+  }
+}
+
+export class Profile extends jspb.Message {
+  hasIdentity(): boolean;
+  clearIdentity(): void;
+  getIdentity(): oojob_protobuf_system_pb.Identifier | undefined;
+  setIdentity(value?: oojob_protobuf_system_pb.Identifier): void;
+
+  getGivenName(): string;
+  setGivenName(value: string): void;
+
+  getMiddleName(): string;
+  setMiddleName(value: string): void;
+
+  getFamilyName(): string;
+  setFamilyName(value: string): void;
+
+  getUsername(): string;
+  setUsername(value: string): void;
+
+  hasEmail(): boolean;
+  clearEmail(): void;
+  getEmail(): oojob_protobuf_system_pb.Email | undefined;
+  setEmail(value?: oojob_protobuf_system_pb.Email): void;
+
+  getGender(): string;
+  setGender(value: string): void;
+
+  hasBirthdate(): boolean;
+  clearBirthdate(): void;
+  getBirthdate(): google_protobuf_timestamp_pb.Timestamp | undefined;
+  setBirthdate(value?: google_protobuf_timestamp_pb.Timestamp): void;
+
+  getCurrentPosition(): string;
+  setCurrentPosition(value: string): void;
+
+  hasEducation(): boolean;
+  clearEducation(): void;
+  getEducation(): Education | undefined;
+  setEducation(value?: Education): void;
+
+  hasAddress(): boolean;
+  clearAddress(): void;
+  getAddress(): oojob_protobuf_place_pb.Address | undefined;
+  setAddress(value?: oojob_protobuf_place_pb.Address): void;
+
+  hasMetadata(): boolean;
+  clearMetadata(): void;
+  getMetadata(): oojob_protobuf_metadata_pb.Metadata | undefined;
+  setMetadata(value?: oojob_protobuf_metadata_pb.Metadata): void;
+
+  serializeBinary(): Uint8Array;
+  toObject(includeInstance?: boolean): Profile.AsObject;
+  static toObject(includeInstance: boolean, msg: Profile): Profile.AsObject;
+  static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+  static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
+  static serializeBinaryToWriter(message: Profile, writer: jspb.BinaryWriter): void;
+  static deserializeBinary(bytes: Uint8Array): Profile;
+  static deserializeBinaryFromReader(message: Profile, reader: jspb.BinaryReader): Profile;
+}
+
+export namespace Profile {
+  export type AsObject = {
+    identity?: oojob_protobuf_system_pb.Identifier.AsObject,
+    givenName: string,
+    middleName: string,
+    familyName: string,
+    username: string,
+    email?: oojob_protobuf_system_pb.Email.AsObject,
+    gender: string,
+    birthdate?: google_protobuf_timestamp_pb.Timestamp.AsObject,
+    currentPosition: string,
+    education?: Education.AsObject,
+    address?: oojob_protobuf_place_pb.Address.AsObject,
+    metadata?: oojob_protobuf_metadata_pb.Metadata.AsObject,
+  }
+}
+
+export class ConfirmProfileRequest extends jspb.Message {
+  getUsername(): string;
+  setUsername(value: string): void;
+
+  getEmail(): string;
+  setEmail(value: string): void;
+
+  getToken(): string;
+  setToken(value: string): void;
+
+  serializeBinary(): Uint8Array;
+  toObject(includeInstance?: boolean): ConfirmProfileRequest.AsObject;
+  static toObject(includeInstance: boolean, msg: ConfirmProfileRequest): ConfirmProfileRequest.AsObject;
+  static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+  static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
+  static serializeBinaryToWriter(message: ConfirmProfileRequest, writer: jspb.BinaryWriter): void;
+  static deserializeBinary(bytes: Uint8Array): ConfirmProfileRequest;
+  static deserializeBinaryFromReader(message: ConfirmProfileRequest, reader: jspb.BinaryReader): ConfirmProfileRequest;
+}
+
+export namespace ConfirmProfileRequest {
+  export type AsObject = {
+    username: string,
+    email: string,
+    token: string,
+  }
+}
+
+export class ReadProfileRequest extends jspb.Message {
+  getAccountId(): string;
+  setAccountId(value: string): void;
+
+  serializeBinary(): Uint8Array;
+  toObject(includeInstance?: boolean): ReadProfileRequest.AsObject;
+  static toObject(includeInstance: boolean, msg: ReadProfileRequest): ReadProfileRequest.AsObject;
+  static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+  static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
+  static serializeBinaryToWriter(message: ReadProfileRequest, writer: jspb.BinaryWriter): void;
+  static deserializeBinary(bytes: Uint8Array): ReadProfileRequest;
+  static deserializeBinaryFromReader(message: ReadProfileRequest, reader: jspb.BinaryReader): ReadProfileRequest;
+}
+
+export namespace ReadProfileRequest {
+  export type AsObject = {
+    accountId: string,
+  }
+}
+
+export class ValidateUsernameRequest extends jspb.Message {
+  getUsername(): string;
+  setUsername(value: string): void;
+
+  serializeBinary(): Uint8Array;
+  toObject(includeInstance?: boolean): ValidateUsernameRequest.AsObject;
+  static toObject(includeInstance: boolean, msg: ValidateUsernameRequest): ValidateUsernameRequest.AsObject;
+  static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+  static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
+  static serializeBinaryToWriter(message: ValidateUsernameRequest, writer: jspb.BinaryWriter): void;
+  static deserializeBinary(bytes: Uint8Array): ValidateUsernameRequest;
+  static deserializeBinaryFromReader(message: ValidateUsernameRequest, reader: jspb.BinaryReader): ValidateUsernameRequest;
+}
+
+export namespace ValidateUsernameRequest {
+  export type AsObject = {
+    username: string,
+  }
+}
+
+export class ValidateEmailRequest extends jspb.Message {
+  getEmail(): string;
+  setEmail(value: string): void;
+
+  serializeBinary(): Uint8Array;
+  toObject(includeInstance?: boolean): ValidateEmailRequest.AsObject;
+  static toObject(includeInstance: boolean, msg: ValidateEmailRequest): ValidateEmailRequest.AsObject;
+  static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+  static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
+  static serializeBinaryToWriter(message: ValidateEmailRequest, writer: jspb.BinaryWriter): void;
+  static deserializeBinary(bytes: Uint8Array): ValidateEmailRequest;
+  static deserializeBinaryFromReader(message: ValidateEmailRequest, reader: jspb.BinaryReader): ValidateEmailRequest;
+}
+
+export namespace ValidateEmailRequest {
+  export type AsObject = {
+    email: string,
+  }
+}
+
